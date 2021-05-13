2f61f35b2979cc318485aeee31e40eeedb3cdf42
agnsal <agneses92@hotmail.it>
2021-05-13 11:14:17 +0200

    Laboratories added.

--- 1 - Operators/2 - Comparison Operators.py
+++ 1 - Operators/2 - Comparison Operators.py
@@ -8,3 +8,4 @@ print(x >= 0)
 print(x <= 0)
 
 # ToDo: Use if statements to make more detailed prints.
+# ToDo: Check if x in in one of the following intervals: [-2, 5], (10, 100], (200, 300)
---
+++ 1 - Operators/3 - Identity Operators.py
@@ -0,0 +1,6 @@
+x = 1
+y = 1
+z = 2
+
+print(y is x)
+print(y is not z)
