b3c26faa2e10008750e9ee4c701a79608335aa0b
Agnese Salutari <agneses92@hotmail.it>
2021-05-14 18:15:48 +0200

    Add files via upload

---
+++ 1 - Operators/0 - Arithmetic Operators.py
@@ -0,0 +1,35 @@
+
+'''
+Copyright 2021 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License
+'''
+
+x = 4
+
+print("x =", x)
+print("+")
+print(" x + 1 =", x + 1)
+print("-")
+print(" x - 5.7 =", x - 5.7)
+print("*")
+print(" x * 3 =", x * 3)
+print("/")
+print(" x / 3 =", x / 3)
+print("//")
+print(" x // 3 =", 4 // 3)
+print("%")
+print(" x % 3 =", 4 % 3)
+print("**")
+print(" x ** 2 =", x ** 2)
+print("-x")
+print(" -x =", -x)
+
+# ToDo: Create a functions to perform these operations on inputs.
---
+++ 1 - Operators/1 - Assignment Operators.py
@@ -0,0 +1,40 @@
+
+'''
+Copyright 2021 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License
+'''
+
+print("=")
+x = 4
+print(x)
+print("+=")
+x += 1
+print(x)
+print("-=")
+x -= 1
+print(x)
+print("*=")
+x *= 2
+print(x)
+print("/=")
+x /= 2
+print(x)
+print("//=")
+x //= 0.8
+print(x)
+print("%=")
+x %= 3
+print(x)
+print("**=")
+x **= 2
+print(x)
+
+# ToDo: Use assign operators in 1.0 ToDo.
