50563283e3a95311eb469c15cdecf0ab9a293150
agnsal <agneses92@hotmail.it>
2021-05-17 09:57:28 +0200

    Laboratories added.

---
+++ 3 - Sequences/0 - Indexing and Slicing.py
@@ -0,0 +1,24 @@
+s = 'python'
+l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+s += " is a programming language"
+l += [10, 11, 12, 13, 14, 15]
+
+print(f"s: {s}")
+print(f"l: {l}")
+print(f"s[0]: {s[0]}")
+print(f"l[0]: {l[0]}")
+print(f"s[-1]: {s[-1]}")
+print(f"l[-1]: {l[-1]}")
+print(f"s[1:5]: {s[1:5]}")
+print(f"l[1:5]: {l[1:5]}")
+print(f"s[:3]: {s[:3]}")
+print(f"l[:3]: {l[:3]}")
+print(f"s[:-3]: {s[:-3]}")
+print(f"l[:-3]: {l[:-3]}")
+print(f"s[0:8:2]: {s[0:8:2]}")
+print(f"l[0:8:2]: {l[0:8:2]}")
+print(f"s[::-3]: {s[:-3]}")
+print(f"l[::-3]: {l[:-3]}")
+
+# ToDo: read a string from a text file and print its last 10 chars
