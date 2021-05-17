70c87a6ec0dcc1275716a5fe97aa9e98ae8f6fc7
agnsal <agneses92@hotmail.it>
2021-05-17 17:11:59 +0200

    Laboratories added.

---
+++ 5 - Statements/0 - If, then, else.py
@@ -0,0 +1,32 @@
+
+'''
+Copyright 2021 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License
+'''
+
+x = 1
+y = 2
+z = 3
+
+if x != 0:
+    print("x is not 0")
+
+if y == 0:
+    print("y is 0")
+else:
+    print("y is not 0")
+
+if z is 0:
+    print("z is 0")
+elif z == 100:
+    print("z is not 0, but z is 100")
+else:
+    print("z is not 0 and z is not 100")
