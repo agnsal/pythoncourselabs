6e41a6d8bf1b2aefd892ec014866d05e21dd6604
Agnese Salutari <agneses92@hotmail.it>
2021-05-17 18:32:40 +0200

    Add files via upload

---
+++ 5 - Statements/3 - For.py
@@ -0,0 +1,39 @@
+
+'''
+Copyright 2021 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License
+'''
+
+l = [0, 1, 2, 3, 4, 5]
+for x in l:
+    print(x)
+print("The end")
+
+d = {'ita': 'ciao', 'eng': 'hello'}
+for y in d:
+    print(d[y])
+print('The End')
+
+s = 'python'
+for z in s:
+    print(z)
+print('The End')
+
+for a in range(0, 4):
+    print(f"a: {a}")
+print('The End')
+
+for b in range(0, 10, 3):
+    print(f"b: {b}")
+print('The End')
+
+myList = [n for n in range(0, 10) if n % 2 == 0]
+print(f"myList: {myList}")
