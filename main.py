5c9643dfc5c9e4c585fa2652cc692f09d116688a
agnsal <agneses92@hotmail.it>
2021-05-21 12:34:02 +0200

    Modified.

--- main.py
+++ main.py
@@ -2,11 +2,36 @@ import sys
 from test import Test
 
 
+def printTest():
+    print("test!!!!")
+    return 4
+
+
+def argsF(a, b, *args):
+    print(f"a: {a}")
+    print(f"b: {b}")
+    print(f"args: {args}")
+    print(f"type di args: {type(args)}")
+
+
+def kwargsF(a, b, **kwargs):
+    print(f"a: {a}")
+    print(f"b: {b}")
+    print(f"kwargs: {kwargs}")
+    print(f"type di args: {type(kwargs)}")
+
+
 def main(x):
-    t = Test(x)
+    # t = Test(x)
+    # kwargsF(b=1, a=2, c=3, z=4, l=5, y=6)
+    pt = printTest
+    # ...
+    print(type(pt))
+    pt()
+    print(type(pt()))
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    x = sys.argv[1]
-    main(x)
+    # print(sys.argv)
+    # x = sys.argv[1]
+    main("ciao")
--- test/__init__.py
+++ test/__init__.py
@@ -1,15 +1,68 @@
-from test.Utils import printStr
+#from test.Utils import printStr
 
 class Test:
     __myVar = None
+    pub = 1
 
     def __init__(self, x):
         self.setMyVal(x)
         pass
 
     def setMyVal(self, x):
-        self.__myVar = x
-        printStr(self.__myVar)
+        self.__myVar = str(x)
+        # printStr(self.__myVar)
 
     def getMyVal(self):
         return self.__myVar
+
+
+# verificaIta = Test("Titolo: ita")
+# verificaMat = Test("Titolo: mat")
+# print(verificaIta.getMyVal())
+# print(verificaMat.getMyVal())
+# # t.pub = 10
+# # myVal = t.getMyVal()
+# # print(type(t))
+
+class Veicolo:
+    __className = "Veicolo"
+
+    def __init__(self, targa):
+        self.__targa = targa
+
+    def changeTarga(self, newTarga):
+        self.__targa = str(newTarga)
+
+    def getTarga(self):
+        return f"Targa: {self.__targa}"
+
+
+class Automobile(Veicolo, Test):
+    __className = "Automobile"
+
+    def __init__(self, targa, marca):
+        self.__marca = marca
+        super().__init__(targa)
+
+    def getMarca(self):
+        return self.__marca
+
+    def getClassName(self):
+        return self.__className
+
+
+# v = Veicolo("123")
+# print(v.getTarga())
+# v.changeTarga(456)
+# print(v.getTarga())
+a = Automobile("789", "x")
+# print(a.getTarga())
+# print(a.getMarca())
+
+# print(v.__className)
+# print(a.__className)
+#
+# a.__className = "hackedClassName"
+# print(a.getClassName())
+
+print(a.getMyVal())
