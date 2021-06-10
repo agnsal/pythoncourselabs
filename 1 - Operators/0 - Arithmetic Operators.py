2ed0843dde557c2058ef66c35829051a5c5e6618
agnsal <agneses92@hotmail.it>
2021-06-10 12:51:52 +0200

    07.06.21

--- 1 - Operators/0 - Arithmetic Operators.py
+++ 1 - Operators/0 - Arithmetic Operators.py
@@ -12,24 +12,85 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 See the License for the specific language governing permissions and limitations under the License
 '''
 
-x = 4
-
-print("x =", x)
-print("+")
-print(" x + 1 =", x + 1)
-print("-")
-print(" x - 5.7 =", x - 5.7)
-print("*")
-print(" x * 3 =", x * 3)
-print("/")
-print(" x / 3 =", x / 3)
-print("//")
-print(" x // 3 =", 4 // 3)
-print("%")
-print(" x % 3 =", 4 % 3)
-print("**")
-print(" x ** 2 =", x ** 2)
-print("-x")
-print(" -x =", -x)
+# x = 4
+#
+# print("x =", x)
+# print("+")
+# print(" x + 1 =", x + 1)
+# print("-")
+# print(" x - 5.7 =", x - 5.7)
+# print("*")
+# print(" x * 3 =", x * 3)
+# print("/")
+# print(" x / 3 =", x / 3)
+# print("//")
+# print(" x // 3 =", 4 // 3)
+# print("%")
+# print(" x % 3 =", 4 % 3)
+# print("**")
+# print(" x ** 2 =", x ** 2)
+# print("-x")
+# print(" -x =", -x)
 
 # ToDo: Create a functions to perform these operations on inputs.
+
+
+def somma(a, b):
+    return a + b
+
+
+def differenza(a, b):
+    return a - b
+
+
+def moltiplicazione(a, b):
+    return a * b
+
+
+# def operation(a, b, op):
+#     x = None
+#     if op == "+":
+#         x = a + b
+#     elif op == "-":
+#         x = a - b
+#     elif op == "*":
+#         x = a * b
+#     elif op == "/":
+#         x = a / b
+#     else:
+#         print("oparatore non valido")
+#     print(x)
+#
+#
+# x = int(input("scrivi il 1 nunero"))
+# y = int(input("scrivi il 2 nunero"))
+# z = input("Scrivi un operatore")
+# operation(x, y, z)
+
+
+def operation(a, b, op):
+    if op == "addizione":
+        return a + b
+    elif op == "sottrazione":
+        return differenza(a, b)
+    elif op == "prodotto":
+        return moltiplicazione(a, b)
+    else:
+        print("Operazione non consentita!!!")
+
+
+# print(somma(1, 2))
+# print(operation(1, 2, "addizione"))
+# print(operation(2, 3, "prodotto"))
+# print(operation(2, 3, "ciao"))
+
+
+operationMapping = {
+    "addizione": somma,
+    "sottrazione": differenza,
+    "prodotto": moltiplicazione
+}
+
+funzioneDaEseguire = operationMapping['addizione']
+print(funzioneDaEseguire, type(funzioneDaEseguire))
+print(funzioneDaEseguire(2, 3))
