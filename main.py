b5cf054deb64fb0dea0daf0e3f54846f83127bf7
agnsal <agneses92@hotmail.it>
2022-09-22 17:33:01 +0200

    Update 20.09.22

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import numpy
 # import dictExplorer  # from dictExplorer import *  # 1
 from dictExplorer import esplora2  # 2
-from dictExplorer import esplora2 as e
+from dictExplorer import esplora2 as e  # 3
 # from dictExplorer import esplora, esplora2
 
 if __name__ == "__main__":
     x = {'test': 'ciao', 'y': {'z': [1, 2, 3]}}
     # dictExplorer.esplora(x)  # 1
     # esplora2(x)  # 2
-    e(x)
+    e(x)  # 3
+
+    # Callable
+    print(e, id(e))
+    funzioneCallable = e
+    print(funzioneCallable, id(funzioneCallable))
+    funzioneCallable(x)
 
