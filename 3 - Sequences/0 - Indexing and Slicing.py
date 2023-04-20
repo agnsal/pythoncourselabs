4389b74e268f5e182954a862ba1cebdfeb6511d5
agnsal <agneses92@hotmail.it>
2023-04-20 10:15:08 +0200

    2023 update

--- 3 - Sequences/0 - Indexing and Slicing.py
+++ 3 - Sequences/0 - Indexing and Slicing.py
@@ -1,6 +1,6 @@
 
 '''
-Copyright 2021-2022 Agnese Salutari.
+Copyright 2021-2023 Agnese Salutari.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
