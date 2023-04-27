734cc9f144d7c872be407e4957ea74779c0329e2
agnsal <agneses92@hotmail.it>
2023-04-27 18:01:15 +0200

    2023 update

---
+++ 9 - Project/main.py
@@ -0,0 +1,30 @@
+
+'''
+Copyright 2021-2023 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License
+'''
+
+
+def main():
+    # ToDo: Get data from the given csv files, once using csv, once using Pandas
+    # ToDo: Put the data in a list of dicts and print it
+    # ToDo: Convert any csv file into an xlsx file (using Python)
+    # ToDo - fitness.csv: Calculate the average duration time, all the burnt calories, the lowest value of pulse and the biggest value of maxpulse
+    # ToDo - rdu-weather-history.csv: Calculate average tmax, average tmin, total prcp, total snow, oldest date, last date
+    # ToDo - Demographic_Statistics_By_Zip_Code.csv: Calculate total female and male percentages and make a pie chart
+    # ToDo: Write all the results in out.txt file (with Python)
+    # ToDo: Write the results using Python logging
+    # ToDo (finally): Now use a venv
+    pass
+
+
+if __name__ == "__main__":
+    main()
