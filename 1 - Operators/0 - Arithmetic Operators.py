5f4d0803395cf3ee2cca924724c3dccd4e8676d2
agnsal <agneses92@hotmail.it>
2023-05-04 17:47:03 +0200

    04.05.23

--- 1 - Operators/0 - Arithmetic Operators.py
+++ 1 - Operators/0 - Arithmetic Operators.py
@@ -12,24 +12,51 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 See the License for the specific language governing permissions and limitations under the License
 '''
 
-x = 4
-
-print("x =", x)
-print("+")
-print(" x + 1 =", x + 1)
-print("-")
-print(" x - 5.7 =", x - 5.7)
-print("*")
-print(" x * 3 =", x * 3)
-print("/")
-print(" x / 3 =", x / 3)
-print("//")
-print(" x // 3 =", 4 // 3)
-print("%")
-print(" x % 3 =", 4 % 3)
-print("**")
-print(" x ** 2 =", x ** 2)
-print("-x")
-print(" -x =", -x)
+# x = 4
+#
+# print("x =", x)
+# print("+")
+# print(" x + 1 =", x + 1)
+# print("-")
+# print(" x - 5.7 =", x - 5.7)
+# print("*")
+# print(" x * 3 =", x * 3)
+# print("/")
+# print(" x / 3 =", x / 3)
+# print("//")
+# print(" x // 3 =", 4 // 3)
+# print("%")
+# print(" x % 3 =", 4 % 3)
+# print("**")
+# print(" x ** 2 =", x ** 2)
+# print("-x")
+# print(" -x =", -x)
 
 # ToDo: Create a functions to perform these operations on inputs.
+
+
+def calcolo(x):
+    print("x =", x)
+    print("+")
+    print(" x + 1 =", x + 1)
+    print("-")
+    print(" x - 5.7 =", x - 5.7)
+    print("*")
+    print(" x * 3 =", x * 3)
+    print("/")
+    print(" x / 3 =", x / 3)
+    print("//")
+    print(" x // 3 =", 4 // 3)
+    print("%")
+    print(" x % 3 =", 4 % 3)
+    print("**")
+    print(" x ** 2 =", x ** 2)
+    print("-x")
+    print(" -x =", -x)
+
+
+check = [str(c) for c in range(1, 101)]
+numero = ''
+while numero not in check:
+    numero = input("Inserisci un numero tra 1 e 100:\n")
+calcolo(int(numero))
--- test/myFile.py
+++ test/myFile.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 # print("Ciao!")
 # x = 10
 # y = 4
@@ -145,22 +148,188 @@
 #     print(x, ' è amico di ', myList[posizione - 1])
 
 
-s = "Ciao Python!!!"
-delta = 1
-# Cifro
-c = ''
-for lettera in s:
-    o = ord(lettera)
-    print(lettera, ' - ', o, ' - ', chr(o))
-    newOrd = o - delta
-    c += chr(newOrd)
-print(c)
+# s = "Ciao Python!!!"
+# delta = 2
+# # Cifro
+# c = ''
+# for lettera in s:
+#     o = ord(lettera)
+#     print(lettera, ' - ', o, ' - ', chr(o))
+#     newOrd = o - delta
+#     c += chr(newOrd)
+# print(c)
+#
+# # Decifro
+# d = 0
+# while True:
+#     t = ''
+#     for lettera in c:
+#         o = ord(lettera)
+#         newOrd = o + d
+#         t += chr(newOrd)  # t += chr(o + d) oppure t += chr(ord(lettera + d))
+#     print(d, ': ', t)
+#     if not input("Procedo? ").strip():
+#         break
+#     else:
+#         d += 1
+# print(t)
 
-# Decifro
-t = ''
-for lettera in c:
-    o = ord(lettera)
-    newOrd = o + delta
-    t += chr(newOrd)
-print(t)
 
+def laMiaFunzione():
+    print("Ciao Python!")
+#
+#
+# def laMiaFunzioneConParam(myStr):
+#     print(f"Il parametro è: {myStr}")
+#     print("Fine funzione!")
+#
+#
+# def laMiaFunzioneConParams(a="sono a", b="sono b", c="sono c"):
+#     print(f"I parametri sono: '{a}', '{b}' e \"{c}\"")
+#     print("Fine funzione!")
+#
+#
+# print("Prima della funzione")
+# laMiaFunzioneConParams(c="sono la nuova c!", b="sono il sostituto di b", a="sono la nuova a")
+#
+# print("Dopo la funzione")
+
+# def argsFunction(x, y, *args):
+#     print(args)
+#     print(type(args))
+#     s = 0
+#     for a in args:
+#         print(f"{a} è uno degli args")
+#         s += a
+#     print(f"La somma degli args è: {s}")
+#     print(x, ' - ', y)
+
+
+# def kwargsFunction(x, **kwargs):
+#     print(f"x: {x}")
+#     print(kwargs)
+#     print(type(kwargs))
+#     for i in kwargs.items():
+#         print(i)
+#     laMiaFunzione()
+#     myRis = (100, 1)
+#     return myRis
+#
+#
+# x, y = kwargsFunction("sono x", b=2, a=1, z=3, c=20)
+# print(x, ' - ', y)
+
+
+# x = 100
+# print(f"Inizio - x = {x}")
+#
+# def raddoppia(myInt):
+#     x = myInt * 2
+#     print(f"Raddoppia - x = {x}")
+#     return x
+#
+# risultato = raddoppia(x)
+# print(f"Risultato: {risultato}")
+# print(f"Fine - x = {x}")
+# x = risultato
+# x = raddoppia(20)
+# print(f"Fine 2 - x = {x}")
+
+
+# def raddoppiaLista(l):
+#     r = []
+#     for elem in l:
+#         r.append(elem * 2)
+#     return r
+
+# myList = [1, 2, 3]
+# risultato = raddoppiaLista(myList)
+# print(f"Risultato: {risultato}")
+# print(f"MyList: {myList}")
+
+# def somma(x):
+#     x += 1000
+#     # print(x)
+#     return x
+#
+# def raddoppiaDizionario(d):
+#     for k in d:
+#         d[k] = 2 * d[k]
+#     return d
+#
+# myVar = 10
+# print(f"Inizio - myVar: {myVar}")
+# risultato = somma(myVar)
+# print(risultato)
+# print(f"Fine - myVar: {myVar}")
+#
+# print("########")
+# myDiz = {"primo": 1, "secondo": 2, "terzo": 3}
+# print(f"Inizio - myDiz: {myDiz}")
+# risultato1 = raddoppiaDizionario(myDiz.copy())
+# print(risultato1)
+# print(f"Fine - MyDiz: {myDiz}")
+# print(myDiz is risultato1)  # id(myDiz) == id(risultato1)
+
+# def myFun():
+#     print("Funzione eseguita!")
+#
+# mf = myFun
+# print(mf, ' - ', type(mf))
+# myFun()
+# mf()
+
+
+# def somma(a, b):
+#     return a + b
+#
+# print(somma(10, 20))
+#
+# s = lambda a, b: a + b
+# print(s(10, 20))
+
+
+#
+# def test():
+#     x = "ciao"
+#     y = "Python"
+#     print('Funzione: ', x, ' - ', y)
+#     print('Funzione: ', id(x), ' - ', id(y))
+#     risultato = x + y
+#     return risultato
+#
+# def nuovoTest():
+#     x = "ciao"
+#     y = "Python"
+#     print('Funzione: ', x, ' - ', y)
+#     print('Funzione: ', id(x), ' - ', id(y))
+#     risultato = x + y
+#     return risultato
+#
+#
+# x = 10
+# y = 100
+# print('Dichiarazione: ', x, ' - ', y)
+# print('Dichiarazione: ', id(x), ' - ', id(y))
+# test()
+# print('Dopo la funzione: ', x, ' - ', y)
+# print('Dopo la funzione: ', id(x), ' - ', id(y))
+# output = test()
+# print(output)
+
+# def getMin(a, b):
+#     res = a
+#     if a > b:
+#         res = b
+#     return res
+
+# l = [10, 100, 4, 1000]
+#
+# minNum = l[0]
+# for item in l[1:]:
+#     minNum = min(minNum, item)
+# print(minNum)
+#
+# print(reduce(lambda a, b: min(a, b), l))
+
+print(reduce(lambda a, b: min(a, b), [10, 100, 4, 1000]))
